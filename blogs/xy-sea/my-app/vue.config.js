b12c1d0ebf46af7f724ebb020293e15ad61d4d5c
A SOUL <59335580+140948940@users.noreply.github.com>
2024-09-19 14:00:53 +0800

    workflow: gitmodules change

--- blogs/xy-sea/my-app/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  productionSourceMap: false,
-  chainWebpack(config) {
-    config.optimization.minimize(false)
-  },
-})
